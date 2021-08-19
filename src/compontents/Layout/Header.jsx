0a0dc882b57da8337549e7c1a6dad0971a634ede
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-19 08:46:48 +0300

    header styled

--- src/compontents/Layout/Header.jsx
+++ src/compontents/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 const Header = (props) => {
   return (
     <div>
-      <header>
+      <header className={classes.header}>
         <h1>React Meals</h1>
         <button>Cart</button>
       </header>
-      <div>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="table full of food" />
       </div>
     </div>
