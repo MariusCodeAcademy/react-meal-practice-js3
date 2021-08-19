558835b73e3b0802ce643fb9d4323cbf5036b94b
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-19 08:35:04 +0300

    header in progress + meals.jpg

---
+++ src/compontents/Layout/Header.jsx
@@ -0,0 +1,15 @@
+const Header = (props) => {
+  return (
+    <div>
+      <header>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
