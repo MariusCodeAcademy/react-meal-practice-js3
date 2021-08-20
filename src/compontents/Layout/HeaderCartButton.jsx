942396e4fd2c22694cf0ba26ebd0b272bbabb95d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-20 09:27:45 +0300

    useReducer start, default state

--- src/compontents/Layout/HeaderCartButton.jsx
+++ src/compontents/Layout/HeaderCartButton.jsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // TODO: count all items quantities
+  const numberOfCartItems = cartCtx.items.length;
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
