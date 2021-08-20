afd52afbde396169f6e020734d3cc8e9e7a8d1da
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-20 08:23:18 +0300

    showCartBtn state, open modal works

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import "./App.css";
 import Header from "./compontents/Layout/Header";
 import Meals from "./compontents/Meals/Meals";
 import Cart from "./compontents/Cart/Cart";
+import { useState } from "react";
+
+// uzduotis panaudoti state kintamaji ir valdyti modalo isjungima ir ijungima su
+// atitinkamais mygtukais ir paspaudimais
+
+// su diagrams net, nubraizyti sio projekto shemini vaizda
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <div className="App">
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
