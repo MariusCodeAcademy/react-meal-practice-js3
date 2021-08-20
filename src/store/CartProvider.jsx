0575c5ad4f205db2c945129aa1e70581744185dd
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-20 09:49:24 +0300

    reducer fn ADD case handled

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -6,8 +6,24 @@ const defaultCartState = {
   totalAmount: 0,
 };
 
+// pagrindine reducer funkcija ==================
+// action.item === { id: "c1", name: "Sushi", amount: 2, price: 12.99 }
 const cartReducer = (state, action) => {
-  return defaultCartState;
+  switch (action.type) {
+    case "ADD":
+      // visa pridejimo i krepseli logika ir grazinti nauja sate versija
+      const { item } = action;
+      const updatedItems = [...state.items, item];
+      const updatedTotalAmount = state.totalAmount + item.price * item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    case "REMOVE":
+      throw new Error("remove item not completed yet");
+    default:
+      return state;
+  }
 };
 
 const CartProvider = (props) => {
@@ -16,8 +32,13 @@ const CartProvider = (props) => {
     defaultCartState
   );
 
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (item) => {};
+  const addItemToCartHandler = (item) => {
+    // add to cart action
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
