4e35c3d2495e544bb1fcecde5d98cfde13de9a21
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-23 10:48:26 +0300

    I can do this uzd Arr

---
+++ src/tut/IcanDoThis.js
@@ -0,0 +1,30 @@
+const items = [
+  { id: "m1", name: "Sushi", amount: 1, price: 22.99 },
+  { id: "m2", name: "Schnitzel", amount: 3, price: 16.5 },
+  { id: "m3", name: "Barbecue Burger", amount: 1, price: 12.99 },
+];
+
+// parasyti funkicja kuri ima masyva ir id kaip argumenta
+// ir grazina masyva be to nario kurio id buvo paduota
+
+function removeOne(arr, id) {
+  console.log("delete ran");
+}
+
+// delete (items, m1); // =>
+// [
+//    { id: "m2", name: "Schnitzel", amount: 3, price: 16.5 },
+//    { id: "m3", name: "Barbecue Burger", amount: 1, price: 12.99 },
+//]
+
+console.log("test");
+
+function decrOne(arr, id) {}
+// parasyti funkcija kuri pamazina kiek vientetu jei kiekis daugiau uz 1
+
+function delteOrDecrese(arr, id) {
+  // abu dalukus
+  // parasyti funkcija kuri pamazina kiek vientetu jei kiekis daugiau uz 1
+  // parasyti funkicja kuri ima masyva ir id kaip argumenta
+  // ir grazina masyva be to nario kurio id buvo paduota
+}
